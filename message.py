2a7eb1d85e83ee62c16ab38ec623a5baf5daa293
udaybabuk <udaikumar95@gmail.com>
2018-08-03 12:07:45 +0530

    Un Staging in message.py file

--- message.py
+++ message.py
@@ -1,2 +1,6 @@
 print 'Fun'*3
 name = raw_input("What is your name >>>")
+print "Enter your name"
+x = raw_input()
+y = raw_input("age >>>") #age>>> will be printed before input
+print "Your name is",x,"and","and your age is",y,
